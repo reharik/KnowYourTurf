0d63f5bff542f310d2b88f84a9356b882438a407
Raif Harik <reharik@gmail.com>
2013-02-01 18:41:24 -0600

    committing before I branch and rename company

--- DBFluentMigration/Iteration_2/UpdateOperations_200.cs
+++ DBFluentMigration/Iteration_2/UpdateOperations_200.cs
@@ -28,6 +28,17 @@ namespace DBFluentMigration.Iteration_2
             _operations.CreateOperationForMenuItem("Site8");
             _operations.CreateOperationForMenuItem("Site9");
             _operations.CreateOperationForMenuItem("Site10");
+
+            _operations.CreateOperationForMenuItem("Users");
+            _operations.CreateOperationForMenuItem("Clients");
+            _operations.CreateOperationForMenuItem("Site10");
+            _operations.CreateOperationForMenuItem("Site10");
+            _operations.CreateOperationForMenuItem("Site10");
+            _operations.CreateOperationForMenuItem("Site10");
+            _operations.CreateOperationForMenuItem("Site10");
+            _operations.CreateOperationForMenuItem("Site10");
+            _operations.CreateOperationForMenuItem("Site10");
+            _operations.CreateOperationForMenuItem("Site10");
         }
     }
 }
