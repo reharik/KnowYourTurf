0ad04c565e0169e2f1eda1593a65d8c200ae5ed7
Raif Harik <reharik@gmail.com>
2013-03-25 11:52:08 -0500

    fixed bad merge

--- DBFluentMigration/Iteration_1/10110_Alter_Employee_Daily_Task_Sproc.cs
+++ DBFluentMigration/Iteration_1/10110_Alter_Employee_Daily_Task_Sproc.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using FluentMigrator;
 
 namespace DBFluentMigration.Iteration_1
@@ -16,23 +15,4 @@ namespace DBFluentMigration.Iteration_1
         {
         }
     }
-=======
-﻿using FluentMigrator;
-
-namespace DBFluentMigration.Iteration_1
-{
-    [Migration(100110)]
-    public class Alter_Employee_Daily_Task_Sproc : Migration
-    {
-        public override void Up()
-        {
-            string sql = System.IO.File.ReadAllText(@"dbfluentmigration\Alter_EmployeeDailyTasks_Sproc.sql");
-            Execute.Sql(sql);
-        }
-
-        public override void Down()
-        {
-        }
-    }
->>>>>>> e6c768aa5fcde7225dff8a7677f36df214c0183f
 }
