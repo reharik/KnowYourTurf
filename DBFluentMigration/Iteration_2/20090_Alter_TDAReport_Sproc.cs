60e841760e7b588f1ffa88e69b9a04da2ff0d8b7
Raif Harik <reharik@gmail.com>
2013-04-05 20:05:45 -0500

    worked on several bugs

---
+++ DBFluentMigration/Iteration_2/20090_Alter_TDAReport_Sproc.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace DBFluentMigration.Iteration_1
+{
+    [Migration(20090)]
+    public class Alter_TDAReport_Sproc : Migration
+    {
+        public override void Up()
+        {
+            string sql = System.IO.File.ReadAllText(@"dbfluentmigration\Alter_TDAReport_Sproc_20090.sql");
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
